04496e0585cb9b730b4ce4fa8dec7da885ee5913
Bmw65314904 <siripanboonmee46@gmail.com>
2024-11-25 15:23:04 +0700

    Update question3-3.dart

--- question3-3.dart
+++ question3-3.dart
@@ -1,5 +1,1 @@
-void createUser(String name, int age, {bool isActive = true}) {
-  print('ชื่อ: $name');
-  print('อายุ: $age');
-  print('สถานะผู้ใช้งาน: ${isActive ? "Active" : "Inactive"}');
-}
+
