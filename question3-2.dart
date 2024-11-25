ad3ac9644788ae590d2d46c5819e77c496496ef6
Bmw65314904 <siripanboonmee46@gmail.com>
2024-11-25 15:32:46 +0700

    Update question3-2.dart

--- question3-2.dart
+++ question3-2.dart
@@ -1,1 +1,16 @@
-
+import 'dart:io';
+ 
+bool isEven(int number) {
+  return number % 2 == 0;
+}
+ 
+void main() {
+  print('กรุณาป้อนตัวเลข:');
+  int number = int.parse(stdin.readLineSync()!);
+ 
+  if (isEven(number)) {
+    print('$number เป็นเลขคู่');
+  } else {
+    print('$number เป็นเลขคี่');
+  }
+}
